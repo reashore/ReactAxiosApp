fa7aed35b8b0fd8a65799be2ef162fb9230aaa08
Frank J. Reashore <frank.reashore@gov.bc.ca>
2018-01-08 16:16:33 -0800

    Working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import React from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.url = this.props.url;
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    if (!this.state.data) {
+      this.getData(this.url)
+        .then(data => this.setState({data}))
+        .catch(error => console.log(error));
+    }
+  }
+
+  async getData(url) {
+    const result = await axios(url);
+    return result.data;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>React Axios App</h1>
+        {this.state.data ? this.formatTable(this.state.data) : "Loading..."}
       </div>
+    );
+  }
+
+  formatTable(dataArray) {
+    return (
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>Vendor Id</th>
+            <th>Name</th>
+            <th>OCG Number</th>
+            <th>Managing QS Area</th>
+            <th>Primary Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataArray.map((data, n) => {
+            return (
+              <tr key={`row ${n}`}>
+                <td>{data.vendorId}</td>
+                <td>{data.name}</td>
+                <td>{data.ocgNumber}</td>
+                <td>{data.managingQsArea}</td>
+                <td>{data.primaryAddress}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     );
   }
 }
